ed0e9038cb021d5f01f3bba662b6f34e80f10412
injamul007 <injamul007hoque@gmail.com>
2025-11-23 23:12:36 +0600

    add table format UI for showing my-parcels data on my-parcels page

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -1,23 +1,53 @@
-import React from 'react';
-import useAuth from '../../../hooks/useAuth';
-import useAxiosSecure from '../../../hooks/useAxiosSecure';
-import { useQuery } from '@tanstack/react-query';
+import React from "react";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const MyParcels = () => {
-  const {user} = useAuth();
-  const axiosSecure  = useAxiosSecure();
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
 
-  const {data: parcels=[]} = useQuery({
+  const { data: parcels = [] } = useQuery({
     queryKey: ["my-parcels", user?.email],
     queryFn: async () => {
-      const res = await axiosSecure.get(`/parcels?email=${user.email}`)
-      return res.data.result
-    }
-  })
+      const res = await axiosSecure.get(`/parcels?email=${user.email}`);
+      return res.data.result;
+    },
+  });
 
   return (
-    <div>
-      <h1 className='text-3xl text-center'>All of My Parcels : {parcels.length || "No Parcel Found"}</h1>
+    <div className="w-11/12 mx-auto">
+      <h1 className="text-3xl text-center my-12">
+        All of My Parcels : {parcels.length || "No Parcel Found"}
+      </h1>
+
+      <div>
+        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Cost</th>
+                <th>Payment Status</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                parcels.map((parcel,idx) => <tr key={parcel._id}>
+                <th>{idx+1}</th>
+                <td>{parcel.parcelName}</td>
+                <td>{parcel.cost}</td>
+                <td>Blue</td>
+                <td>Blue</td>
+              </tr>)
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
